bd0ca1bbcc55e7f01c1dc4961d41ace93ccf6a29
KRochaS <karinerocha.s@hotmail.com>
2023-02-22 15:26:51 -0300

    Comment Component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,60 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+import { Avatar } from './Avatar';
+import { useState } from 'react';
+
+
+export function Comment({ content, onDeleteComment }) {
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+
+
+    function handleLikeComment() {
+        // setLikeCount(likeCount + 1);
+
+        // resolvendo problema de closures
+
+        // const newLikeCount = likeCount + 1
+        // setLikeCount(newLikeCount);
+
+
+        setLikeCount((state) => {
+            return state + 1;
+        });
+    }
+
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://github.com/diego3g.png" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong> Karine Rocha</strong>
+                            <time title="20 de Fevereiro ás 15:30" dateTime="2023-02-20 15:28:00">
+                                Cerca de 2h atrás
+                            </time>
+                        </div>
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>
+                        {content}
+                    </p>
+                </div>
+
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp size={20} />
+                        Aplaudir <span> {likeCount} </span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
